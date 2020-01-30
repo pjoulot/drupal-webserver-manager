dfac1d82b58d9de56d6e894d6fad772f44899ba1
Philippe Joulot <philippe.joulot@laposte.net>
2020-01-29 17:42:43 -0800

    Fix path for hosts file

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ fi
 
 # Write the host in the ansible hosts.
 
-echo "Clean in case an host with the same name has already been created before"
+# Clean in case an host with the same name has already been created before
 sed "/$SERVER_IP/d" ./inventory/webservers/hosts > ./inventory/webservers/hosts
 echo "$SERVER_IP ansible_connection=ssh ansible_user=$SERVER_USER" >> ./inventory/webservers/hosts && echo "Your server $SERVER_IP can now be managed by ansible"
 
@@ -36,4 +36,4 @@ echo "Install required package for using ansible and configure the server."
 ssh -p $DEFAULT_SSH_PORT "$SERVER_USER@$SERVER_IP" "apt-get update; apt-get install python -y;"
 
 echo "Launching the playbook that install all the components."
-ansible-playbook -i inventory/webservers  install.yml --extra-vars "tools=$TOOLS"
+#ansible-playbook -i inventory/webservers  install.yml --extra-vars "server_ip=$SERVER_IP"
